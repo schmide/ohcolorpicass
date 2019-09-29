17b205af072560cc18ca169d6a64af188ab78445
schmide <andrew@schmide.com>
2019-09-28 23:30:41 -0400

    Switched to fixed point

--- colorfade/colorfade.cpp
+++ colorfade/colorfade.cpp
@@ -10,25 +10,28 @@
 #define COLORTYPE short int  
 #define STEPTYPE short int  
 #define DOTTYPE long int  
-#define FLOOR 1
+#define FLOOR 4
+#define SCALOR 128
+#define RATE 32
 
 struct color {
    COLORTYPE r, g, b;
 };
 
-color onColor = { 255, 200 ,0 }, 
-      offColor = { 0, 0, 80 }, 
-      currentColor = offColor;
-STEPTYPE steps = 32;
+STEPTYPE scalor = SCALOR;
+STEPTYPE rate = RATE;
+color onColor = { 255 * SCALOR, 20 * SCALOR , 0 * SCALOR },
+offColor = { 0 * SCALOR, 0 * SCALOR, 255 * SCALOR },
+currentColor = offColor;
 bool flux = true;
 
-void CheckUnderflow(COLORTYPE &delta, COLORTYPE &oldDelta, COLORTYPE &current, COLORTYPE &dest)
+void CheckUnderflow(COLORTYPE &delta, COLORTYPE &span, COLORTYPE &current, COLORTYPE &dest)
 {
    if (abs(delta) < FLOOR) {// underflow compensation 
-      if (oldDelta < 0) {
+      if (span < 0) {
          delta = -FLOOR;
       } else {
-         if (oldDelta > 0) {
+         if (span > 0) {
             delta = FLOOR;
          } else {
             current = dest;
@@ -43,19 +46,19 @@ void UpdateColor()
    colorSpan.r = onColor.r - offColor.r;
    colorSpan.g = onColor.g - offColor.g;
    colorSpan.b = onColor.b - offColor.b;
-   STEPTYPE stepsDirection;
    color dest;
+   color deltaColor;
+   deltaColor.r = colorSpan.r / rate;
+   deltaColor.g = colorSpan.g / rate;
+   deltaColor.b = colorSpan.b / rate;
    if (flux) {
       dest = onColor;
-      stepsDirection = steps;
    } else {
       dest = offColor;
-      stepsDirection = -steps;
+      deltaColor.r = -deltaColor.r ;
+      deltaColor.g = -deltaColor.g ;
+      deltaColor.b = -deltaColor.b ;
    }
-   color deltaColor;
-   deltaColor.r = colorSpan.r / stepsDirection;
-   deltaColor.g = colorSpan.g / stepsDirection;
-   deltaColor.b = colorSpan.b / stepsDirection;
    CheckUnderflow(deltaColor.r, colorSpan.r, currentColor.r, dest.r);
    CheckUnderflow(deltaColor.b, colorSpan.b, currentColor.b, dest.b);
    CheckUnderflow(deltaColor.g, colorSpan.g, currentColor.g, dest.g);
@@ -63,17 +66,25 @@ void UpdateColor()
    vectDot += (dest.g - currentColor.g) * deltaColor.g;
    vectDot += (dest.b - currentColor.b) * deltaColor.b;
    if ( vectDot <= 0 ) {
-      currentColor = dest;
-//      flux = !flux; // no flux given
+      currentColor.r = dest.r;
+      currentColor.g = dest.g;
+      currentColor.b = dest.b;
+      flux = !flux; // no flux given
    } else {
-      currentColor.r += deltaColor.r;
-      currentColor.g += deltaColor.g;
-      currentColor.b += deltaColor.b;
+      currentColor.r = (currentColor.r + deltaColor.r);
+      currentColor.g = (currentColor.g + deltaColor.g);
+      currentColor.b = (currentColor.b + deltaColor.b);
    }
    // integers round down every cycle and dot products are not kind to integers.
    currentColor.r = currentColor.r < 0 ? 0 : currentColor.r;
    currentColor.g = currentColor.g < 0 ? 0 : currentColor.g;
    currentColor.b = currentColor.b < 0 ? 0 : currentColor.b;
+   color outputColor;
+   outputColor.r = currentColor.r / SCALOR;
+   outputColor.g = currentColor.g / SCALOR;
+   outputColor.b = currentColor.b / SCALOR;
+   flux = flux;
+
 }
 
 int main()
